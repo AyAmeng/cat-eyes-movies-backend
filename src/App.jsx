c989ea127b611f8327c512560e7a2bc333309846
Wave <372969383@qq.com>
2017-03-12 10:57:22 +0800

    ww

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import Hello from './components/Hello'
+import { browserHistory as history, Router } from 'react-router'
+import routes from 'routes'
+import logo from './assets/logo.svg'
+import './App.css'
+
+import Counter from 'components/Counter'
+
+export default class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <div className='App-header'>
+          <img src={logo} className='App-logo shake-rotate' alt='logo' />
+        </div>
+        <Counter />
+        <Hello />
+        <Router history={history} routes={routes} key={Math.random()} />
+      </div>
+    )
+  }
+}
